ca1b6d5f0bbb93371b9ad75579a282cfeaaa0737
0nedec <0nedec@github.com>
2024-11-29 23:00:23 -0500

    Created Boilerplate code and makefile.

---
+++ Boilerplate/soln.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+int main(){
+  cout << "Hello BoilerPlate!" << endl;
+  return 0;
+}

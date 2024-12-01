63ea48099ea4621f714cdfe2d9893637bc697052
0nedec <0nedec@github.com>
2024-12-01 13:56:35 -0500

    Updated Boilerplate and gitignore.

--- Boilerplate/soln.cpp
+++ Boilerplate/soln.cpp
@@ -7,7 +7,24 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[]){
+  if (argc != 2){
+    cerr << "Usage: " << argv[0] << "<filename>" << endl;
+    return 1;
+  }
+  string filename = argv[1];
+  ifstream file(filename);
+  string line;
+
+  if (!file.is_open()){
+    cerr << "Error opening file: " << filename << endl;
+    return 1;
+  }
+
+  while(getline(file, line)){
+  //do something
+  }
+
   cout << "Hello BoilerPlate!" << endl;
   return 0;
 }

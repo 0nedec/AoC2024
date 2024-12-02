6bf190a3f998ef4accd8a209f8ccafe7bcea72a2
0nedec <0nedec@github.com>
2024-12-02 11:29:43 -0500

    Started Day2

---
+++ Day2/soln.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+  if (argc != 2){
+    cerr << "Usage: " << argv[0] << "<filename>" << endl;
+    return 1;
+  }
+  string filename = argv[1];
+  ifstream file(filename);
+  string line;
+  vector<vector<int>> lines;
+  
+    
+
+  if (!file.is_open()){
+    cerr << "Error opening file: " << filename << endl;
+    return 1;
+  }
+
+  while(getline(file, line)){
+    //do something
+    istringstream iss(line);
+    vector<int> numbers;
+    int num;
+    while (iss >> num) {
+      numbers.push_back(num);
+    }
+    lines.push_back(numbers);
+  }
+  file.close();
+
+  cout << "Hello BoilerPlate!" << endl;
+  return 0;
+}

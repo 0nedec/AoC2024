62741e0cfc4baca75fc036d64b9bb52c1560e778
Carlos <carlosantoniocedeno@gmail.com>
2024-12-02 12:15:12 -0500

    Parsed input for Day 2

--- Day2/soln.cpp
+++ Day2/soln.cpp
@@ -8,34 +8,40 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-  if (argc != 2){
-    cerr << "Usage: " << argv[0] << "<filename>" << endl;
-    return 1;
-  }
-  string filename = argv[1];
-  ifstream file(filename);
-  string line;
-  vector<vector<int>> lines;
-  
-    
+	if (argc != 2){
+		cerr << "Usage: " << argv[0] << "<filename>" << endl;
+		return 1;
+	}
+	string filename = argv[1];
+	ifstream file(filename);
+	string line;
+	vector< vector<int> > lines;
+		
 
-  if (!file.is_open()){
-    cerr << "Error opening file: " << filename << endl;
-    return 1;
-  }
+	if (!file.is_open()){
+		cerr << "Error opening file: " << filename << endl;
+		return 1;
+	}
 
-  while(getline(file, line)){
-    //do something
-    istringstream iss(line);
-    vector<int> numbers;
-    int num;
-    while (iss >> num) {
-      numbers.push_back(num);
-    }
-    lines.push_back(numbers);
-  }
-  file.close();
+	while(getline(file, line)){
+		//do something
+		istringstream iss(line);
+		int num;
+		vector<int> numbers;
+		while (iss >> num) {
+			numbers.push_back(num);
+		}
+		lines.push_back(numbers);
+	}
+	file.close();
 
-  cout << "Hello BoilerPlate!" << endl;
-  return 0;
+	for (int ii=0;ii < lines.size();++ii){
+		for(int jj = 0; jj < lines[ii].size(); ++jj){
+			cout << lines[ii][jj] << " ";
+		}
+		cout << endl;
+	}
+
+
+	return 0;
 }

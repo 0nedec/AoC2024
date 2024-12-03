f2603864adf0dfcd5f0983343507f876c07828eb
Carlos <carlosantoniocedeno@gmail.com>
2024-12-02 22:29:36 -0500

    Soln2 clean up. WIP

--- Day2/soln2.cpp
+++ Day2/soln2.cpp
@@ -7,6 +7,48 @@
 
 using namespace std;
 
+char safe '-';
+char direction '-';
+
+
+void set-safety(int a, int b) {
+	//find the difference
+	int diff = a - b;
+	int adj_level = abs(diff);
+
+	//check if ascending or descending difference has flipped. if so mark unsafe.
+	if ((diff < 0) && (direction !=  '-' || direction == 'd'){
+		safe = 'u';
+		return;
+	}
+	else if ((diff > 0)	 && (direction != '-' || direction == 'a'){
+		safe = 'u'
+		return;
+	}
+	//check if no difference between two numbers. if so mark unsafe.
+	else if (diff == 0){
+		safe = 'u';
+		return;
+	}
+	//check if two adjajent levels are at least 1 and at most 3
+	else if (adj_level <1 && adj_level >3){
+		safe = 'u';
+		return;
+	}
+	else if ( diff > 0 && direction == '-'){
+		direction = 'd';
+	}
+	else if ( diff < 0 && direction == '-') {
+		direction = 'a';
+	}
+	safe = 's';
+	return;
+}
+
+
+
+/*
+
 bool is_descending(int a, int b) {
 	return ( a - b  > 0 );
 }
@@ -14,6 +56,7 @@ bool is_descending(int a, int b) {
 bool is_ascending(int a, int b) {
 	return ( a - b < 0); 
 }
+*/
 
 int main(int argc, char *argv[]){
 	if (argc != 2){
@@ -25,9 +68,8 @@ int main(int argc, char *argv[]){
 	string line;
 	vector< vector<int> > lines;
 	int safe_counter = 0;		
-	char direction = '0';
-  char safe = 'u';
-
+  char prob_damp = 't';
+	
 	if (!file.is_open()){
 		cerr << "Error opening file: " << filename << endl;
 		return 1;
@@ -48,16 +90,24 @@ int main(int argc, char *argv[]){
 	for (int ii=0;ii < lines.size();++ii){
 		for(int jj = 0; jj < lines[ii].size(); ++jj){    
 			int kk = 0;
-				//check if the line is completely ascending or descending
 					kk=jj+1;
 					if (kk == lines[ii].size()) {
 						break;
 					}
-					else if(is_descending(lines[ii][jj],lines[ii][kk]) && (direction == '0' || direction == 'd')) {
+/*					else if(is_descending(lines[ii][jj],lines[ii][kk]) && (direction == '0' || direction == 'd')) {
 						direction = 'd';
 						int diff= abs(lines[ii][jj]-lines[ii][kk]);
 						if (diff >= 1 && diff <= 3){
 							safe = 's';
+						}
+						else if(is_descending(lines[ii][jj],lines[ii][kk+1]) && (direction == '0' || direction == 'd') && prob_damp == 't') {
+							direction = 'd';
+							prob_damp = 'f';
+							int diff= abs(lines[ii][jj]-lines[ii][kk+1]);
+							if (diff >= 1 && diff <= 3){
+								safe = 's';
+							}
+							jj++;
 						}
 						else {
 							safe = 'u';
@@ -71,6 +121,15 @@ int main(int argc, char *argv[]){
 						if (diff >= 1 && diff <= 3){
 							safe = 's';
 						}
+						else if(is_ascending(lines[ii][jj],lines[ii][kk+1]) && (direction == '0' || direction == 'a') && prob_damp == 't') {
+							direction = 'a';
+							prob_damp = 'f';
+							int diff= abs(lines[ii][jj]-lines[ii][kk+1]);
+							if (diff >= 1 && diff <= 3){
+								safe = 's';
+							}
+							jj++;
+						}
 						else {
 							safe = 'u';
 							break;
@@ -81,14 +140,15 @@ int main(int argc, char *argv[]){
 						break;
 					}
 		}
-		cout << ii+1 << "\t" << direction << endl;
+		cout << ii+1 << "\t" << safe << direction << endl;
 		if ((direction == 'd' || direction =='a') && safe == 's'){
 			safe_counter++;
 		}
 		direction = '0';
 		safe = 'u';
+		prob_damp = 't';
 	}
-
+*/
 	cout << safe_counter;
 	return 0;
 }

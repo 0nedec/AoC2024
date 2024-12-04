eaf40b4e346366736cfdfc7977e5dab70068b138
Carlos <carlosantoniocedeno@gmail.com>
2024-12-04 16:51:11 -0500

    Solution for Day3 part1.

--- Day3/soln.cpp
+++ Day3/soln.cpp
@@ -8,40 +8,69 @@
 
 using namespace std;
 
-void getMulFromLine(string line){
-	regex mulRegex("mul\\(\\d{1,3},\\d{1,3}\\)");
-	smatch match;
-	string::const_iterator start = line.begin();
-
-	while (regex_search(start, line.cend(), match, mulRegex)){
-		cout << "Match found: " << match.str() << endl;
-		start = match.suffix().first;
+void printVector(vector<string> printable){
+	for (const string& item : printable) {
+		cout << item << endl;
 	}
 	return;
 }
 
-using namespace std;
+
+void getMulFromLine(string line, vector<string>& muls){
+	regex mulRegex("mul\\(\\d{1,3},\\d{1,3}\\)");
+	smatch match;
+	string::const_iterator start = line.begin();
+	
+	while (regex_search(start, line.cend(), match, mulRegex)){
+		//cout << "Match found: " << match.str() << endl;
+		muls.push_back(match.str());
+		start = match.suffix().first;
+	}
+	//printVector(muls);
+	return;
+}
+
+int parseAndMultiplyMuls(vector<string> muls) {
+	int sum = 0;
+	for (int ii=0; ii < muls.size();++ii) {
+		cout <<  muls[ii] << endl;
+		size_t start = muls[ii].find('(');
+		size_t comma = muls[ii].find(',');
+		size_t end = muls[ii].find(')');
+
+		if(start != string::npos && comma != string::npos && end != string::npos){
+		int num1 = stoi(muls[ii].substr(start+1, comma - start -1));
+		int num2 = stoi(muls[ii].substr(comma+1, end - comma -1));
+		cout << " Adding "<< num1 << " and " << num2 << endl;
+			sum += num1*num2;
+		}
+	}
+	return sum;
+}
 
 int main(int argc, char *argv[]){
-  if (argc != 2){
-    cerr << "Usage: " << argv[0] << " <filename>" << endl;
-    return 1;
-  }
-  string filename = argv[1];
+	string filename = argv[1];
   ifstream file(filename);
   string line;
+	vector<string> muls;
 
+	if (argc != 2){
+		cerr << "Usage: " << argv[0] << " <filename>" << endl;
+    return 1;
+  }
   if (!file.is_open()){
     cerr << "Error opening file: " << filename << endl;
     return 1;
   }
 
   while(getline(file, line)){
-  //do something
-  getMulFromLine(line);
-
+		//do something
+		getMulFromLine(line, muls); 
 	}
+	printVector(muls);
+	int sum = parseAndMultiplyMuls(muls);
 
-  cout << "Hello BoilerPlate!" << endl;
+	cout << muls.size() << endl;
+	cout << sum << endl;
   return 0;
 }
